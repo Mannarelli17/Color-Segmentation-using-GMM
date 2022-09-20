5a786c2b053fc74a6382678da92b951b6624ca6c
Jeffrey Ran Zhang <jeffreyzhang5@gmail.com>
2022-09-20 19:31:25 -0400

    Add files via upload
    This is the pseudocode for GMM.m, it is not going to work without the other components. The parameters for Model might not be correct as they are set to numbers rather than vectors or matrices.

---
+++ GMM.m
@@ -0,0 +1,37 @@
+function [cluster, d] = GMM(training)
+    %set these to something else
+    tau = 0;
+    K = 0;
+    scaling_factor = 0;
+    gaussian_mean = 0;
+    covariance = 0;
+    %Need to figure out how to store the model
+    if training == true
+        path=dir('/Users/jeffreyzhang/Documents/MATLAB/CMSC426/jzhang45_proj1/train_images/*.jpg');
+        orange_pixels = loadingData(path);
+     [scaling_factor, gaussian_mean, covariance] = trainGMM(orange_pixels, K);
+    else
+        path=dir('/Users/jeffreyzhang/Documents/MATLAB/CMSC426/jzhang45_proj1/test_images/*.jpg');
+        orange_pixels = loadingData(path);
+        cluster = testGMM(scaling_factor, gaussian_mean, covariance, orange_pixels, tau);
+    end
+    d = measureDepth(cluster);
+    plotGMM(scaling_factor, gaussian_mean, covariance);
+end
+
+function orange_pixels = loadingData(path)
+    orange_pixels = zeros(1,3);
+    for i=1:length(path)
+        imagePath=fullfile(path(i).folder, path(i).name);
+        %read the image
+        image = imread(imagePath);
+        BW_image = roipoly(image);
+        image(repmat(~BW_image,[1 1 3])) = 0;
+        image=reshape(image,640*480,3);
+        for pixel = 1:(640*480)
+            if BW_image(pixel) == 1
+                orange_pixels = vertcat(orange_pixels,image(pixel,:));
+            end
+        end 
+    end
+end
