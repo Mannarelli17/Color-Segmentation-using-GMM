b9a4fd8b8cca133e9933e2ad91092a2a7122b27b
Jeffrey Ran Zhang <jeffreyzhang5@gmail.com>
2022-09-21 20:35:25 -0400

    Update GMM.m

--- GMM.m
+++ GMM.m
@@ -6,18 +6,19 @@ function [cluster, d] = GMM
     
     %train
     path=dir('/Users/jeffreyzhang/Documents/MATLAB/CMSC426/jzhang45_proj1/train_images/*.jpg');
-    orange_pixels = loadingData(path);
+    orange_pixels = loadingDataTrain(path);
     [scaling_factors, gaussian_means, covariances] = trainGMM(orange_pixels, K);
     
-    %test
+    %test (might have to change this to include non orange samples)
     path=dir('/Users/jeffreyzhang/Documents/MATLAB/CMSC426/jzhang45_proj1/test_images/*.jpg');
-    orange_pixels = loadingData(path);
-    cluster = testGMM(scaling_factors, gaussian_means, covariances, orange_pixels, tau);
+    
+    pixels = loadingDataTest(path);
+    cluster = testGMM(scaling_factors, gaussian_means, covariances, pixels, tau);
     d = measureDepth(cluster);
     plotGMM(scaling_factors, gaussian_means, covariances);
 end
 
-function orange_pixels = loadingData(path)
+function orange_pixels = loadingDataTraun(path)
     orange_pixels = zeros(1,3);
     for i=1:length(path)
         imagePath=fullfile(path(i).folder, path(i).name);
@@ -32,4 +33,20 @@ function orange_pixels = loadingData(path)
             end
         end 
     end
+    orange_pixels(1, :) = [];
+end
+
+function pixels = loadingDataTest(path)
+    pixels = zeros(1,3);
+    %maybe test for 1 image at a time
+    for i=1:length(path)
+        imagePath=fullfile(path(i).folder, path(i).name);
+        %read the image
+        image = imread(imagePath);
+        BW_image = roipoly(image);
+        image(repmat(~BW_image,[1 1 3])) = 0;
+        image=reshape(image,640*480,3);
+        pixels = [pixels;image]
+    end
+    pixels(1, :) = [];
 end
