c0719a87773122408f54e1b4e7d3d63b45bfd621
A-Diseye <38788188+A-Diseye@users.noreply.github.com>
2022-09-23 19:43:53 -0400

    Measured distance of ball

--- measureDepth.m
+++ measureDepth.m
@@ -1,49 +1,71 @@
 path=dir('./train_images/*.jpg');
-imageStack=zeros(1,3);
-op_lab = zeros(1,3);
-imageStack_lab=zeros(1,3);
-for image=1:length(path)-1
-    image_path=fullfile(path(image).folder, path(image).name);
 
-    image=imread(image_path);
-
-    image_lab = rgb2lab(image);
-
-    sliderBW = (image_lab(:,:,1)>=20)&(image_lab(:,:,1)<=90)&(image_lab(:,:,2)>=15)&(image_lab(:,:,2)<=50)&(image_lab(:,:,3)>=10)&(image_lab(:,:,3)<=45);
-    BW_lab = sliderBW;
-
-    % Initialize output masked image based on input image.
-    maskedRGBImage_lab = image_lab;
-    % Set background pixels where BW is false to zero.
-    maskedRGBImage_lab(repmat(~BW_lab,[1 1 3])) = 0;
-    maskedRGBImage_lab=reshape(maskedRGBImage_lab,640*480,3);
-    %gathering all orange pixels(lab) in an matrix
-    for pixels = 1:(640*480)
-        if BW_lab(pixels) == 1
-            op_lab = vertcat(op_lab,maskedRGBImage_lab(pixels,:));
-        end
-    end
-end
-
-
-prior = 0.5;
-cov_orange = cov(op_lab);
-mean_orange = mean(op_lab);
-threshold = 0.0000003;
-
-distance = zeros(1);
-area_ball = zeros(1);
+distances = zeros(1);
+areas = zeros(1);
 for images = 1:length(path)
     image_path = fullfile(path(images).folder, path(images).name);
     [filepath,name,ext] = fileparts(image_path);
-    distance = vertcat(distance, str2num(name));
+    distances = vertcat(distances, str2num(name));
 
     image = imread(image_path);
     img_lab = rgb2lab(image);
-    % imshow(i_lab);
-    maskedImage = img_lab;
-    img_lab = im2double(img_lab);
-    img_lab = reshape(img_lab,640*480,3);
-    likelihood_orange =mvnpdf([img_lab(:,1) img_lab(:,2) img_lab(:,3)],mean_orange,cov_orange)
-    
+    BW = (img_lab(:,:,1)>=20)&(img_lab(:,:,1)<=90)&(img_lab(:,:,2)>=15)...
+        &(img_lab(:,:,2)<=50)&(img_lab(:,:,3)>=10)&(img_lab(:,:,3)<=45);
+    stats = regionprops('table',BW,'Centroid',...
+    'MajorAxisLength','MinorAxisLength');
+    [val1 index1] = max(stats.MajorAxisLength);
+    [val2 index2] = max(stats.MinorAxisLength);
+    if (index1 ==index2)
+        centers  = stats.Centroid(index1,:);
+        radii = (stats.MajorAxisLength(index1)+stats.MinorAxisLength(index1))/4 ;
+    else
+        radii_1 = (stats.MajorAxisLength(index1)+stats.MinorAxisLength(index1))/4;
+        radii_2 = (stats.MajorAxisLength(index2)+stats.MinorAxisLength(index2))/4;
+        if radii_1>radii_2
+            radii = radii_1;
+            centers = stats.Centroid(index1,:);
+        else
+            radii = radii_2;
+            centers = stats.Centroid(index2,:);
+        end
+    end
+    areas = vertcat(areas, pi*(radii^2));
+end
+distances = distances(2:length(distances));
+areas = areas(2:length(areas));
+
+%relationship between area we see of the ball and distance
+f=fit(areas,distances,'poly4');
+
+%testing
+path=dir('./test_images/*.jpg');
+for images=1:length(path)
+    image_path = fullfile(path(images).folder, path(images).name);
+    [filepath,name,ext] = fileparts(image_path);
+
+    image=imread(image_path);
+    img_lab = rgb2lab(image);
+    BW = (img_lab(:,:,1)>=20)&(img_lab(:,:,1)<=90)&(img_lab(:,:,2)>=15)&...
+        (img_lab(:,:,2)<=50)&(img_lab(:,:,3)>=10)&(img_lab(:,:,3)<=45);
+    stats = regionprops('table',BW,'Centroid',...
+    'MajorAxisLength','MinorAxisLength');
+    [val1 index1] = max(stats.MajorAxisLength);
+    [val2 index2] = max(stats.MinorAxisLength);
+    if (index1 ==index2)
+        centers  = stats.Centroid(index1,:);
+        radii = (stats.MajorAxisLength(index1)+stats.MinorAxisLength(index1))/4 ;
+    else
+        radii_1 = (stats.MajorAxisLength(index1)+stats.MinorAxisLength(index1))/4;
+        radii_2 = (stats.MajorAxisLength(index2)+stats.MinorAxisLength(index2))/4;
+        if radii_1>radii_2
+            radii = radii_1;
+            centers = stats.Centroid(index1,:);
+        else
+            radii = radii_2;
+            centers = stats.Centroid(index2,:);
+        end
+    end
+    area = pi*(radii^2);
+    distance = f.p1*area^4 + f.p2*area^3 + f.p3*area^2 + f.p4*area+f.p5;
+    fprintf('\ndistance for test image %d is %d',images, distance);
 end
