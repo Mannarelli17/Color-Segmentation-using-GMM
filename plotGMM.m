7e7c2fa7ed668fd065808dcb13d33d3edd64a63e
Jeffrey Ran Zhang <jeffreyzhang5@gmail.com>
2022-09-26 18:08:56 -0400

    Should work now

--- plotGMM.m
+++ plotGMM.m
@@ -1,12 +1,16 @@
 function plotGMM(scaling_factors, gaussian_means, covariances)
     mus = gaussian_means;
     covars = covariances;
-    for i = 1:size(mus)
+    display(covars)
+    save covars
+    n = size(mus);
+    n = n(3);
+    for i = 1:n
         %display(mus(i));
         %display(mus(i,2));
         %display(mus(i,3));
         display(covars(i));
-        [x,y,z] = ellipsoid(mus(i, 1),mus(i, 2),mus(i, 3),sqrt(covars(i,1)),sqrt(covars(i,4)),sqrt(covars(i,6)));
+        [x,y,z] = ellipsoid(mus(1,i),mus(2,i),mus(3,i),sqrt(covars(1,1,i)),sqrt(covars(2,2,i)),sqrt(covars(3,3,i)));
         surf(x, y, z);
         hold on
     end
